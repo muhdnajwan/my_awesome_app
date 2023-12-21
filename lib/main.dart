df651a16d9767562dfc2a32ef07280b78c36eed3
muhdnajwan <128878633+muhdnajwan@users.noreply.github.com>
2023-12-21 13:48:05 +0800

    Update main.dart
    changing no favourite yet. to yes favourite yet.

--- lib/main.dart
+++ lib/main.dart
@@ -185,7 +185,7 @@ class FavoritesPage extends StatelessWidget {
 
     if (appState.favorites.isEmpty) {
       return Center(
-        child: Text('No favorites yet.'),
+        child: Text('Yes favorites yet.'),
       );
     }
 
